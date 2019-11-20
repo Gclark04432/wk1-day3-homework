0113caa6204dff2fabb4792a1ea9f2fb5ef611e3
Gary Clark <gclark04432@live.co.uk>
2019-11-20 15:33:33 +0000

    tasks-one-to-five-completed-in-A

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -2,19 +2,40 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #1. Add "Edinburgh Waverley" to the end of the array
 stops << "Edinburgh Waverley"
-p stops
+# p stops
 
 #2. Add "Glasgow Queen St" to the start of the array
 stops.unshift("Glasgow Queen St")
-p stops
+# p stops
 
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+# p stops
 
-
-#4. Print out the index position of "Linlithgow"
+# # #4. Print out the index position of "Linlithgow"
+# stops.select { |stop| stop = "Linlithgow" }     #=> [4, 5, 6]
+# p stop
+#
 #5. Remove "Livingston" from the array using its name
+for stop in stops
+  if stop == "Livingston"
+    stops.delete(stop)
+  end
+end
+# p stops
+
 #6. Delete "Cumbernauld" from the array by index
+
 #7. Print the number of stops there are in the array?
+p "There are #{stops.length()} stops in the array currently"
+
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+
 #9. Reverse the positions of the stops in the array
-#10 Print out all the stops using a for loop
+  stops.reverse!
+
+
+# #10 Print out all the stops using a for loop
+# for stop in stops
+#   p stops[stop
+# end
